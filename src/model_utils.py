6e281d314f2b48bc874454444860ba49f6e2ac66
aaronlf <36794466+aaronlf@users.noreply.github.com>
2024-03-10 00:38:43 -0300

    Added RNN model

--- src/model_utils.py
+++ src/model_utils.py
@@ -4,9 +4,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.metrics import confusion_matrix, classification_report
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, SimpleRNN, LSTM, GRU, Dense, Input, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.regularizers import l2
+
 from text_utils import preprocess_text
 
-# Function to plot confusion matrix
+
 def plot_cf_matrix(test, pred, model_name):
     cf_matrix = confusion_matrix(test, pred)
     cf_matrix_normalized = cf_matrix.astype('float') / cf_matrix.sum(axis=1)[:, np.newaxis]
@@ -27,13 +33,11 @@ def plot_cf_matrix(test, pred, model_name):
     plt.title(f"Confusion Matrix - {model_name}", fontdict={'size':18}, pad=20)
     plt.show()
 
-# Function to evaluate models
 def evaluate(model, X_test_vec, y_test,model_name):
     y_pred = model.predict(X_test_vec)
     print(classification_report(y_test, y_pred))
     plot_cf_matrix(y_test, y_pred, model_name)
 
-# Function to predict sentiment from texts
 def predict_sentiment(vectorizer, model, texts):
     texts_processed = preprocess_text(texts)
     text_data = vectorizer.transform(texts_processed)
@@ -43,3 +47,16 @@ def predict_sentiment(vectorizer, model, texts):
 
     df = pd.DataFrame({'text': texts, 'sentiment': sentiment_labels})
     return df
+
+def build_simple_RNN_model(input_length, learning_rate=0.001, vocab_size=10000, embedding_dim=100):
+    model = Sequential()
+    model.add(Input(shape=(input_length,)))
+    model.add(Embedding(input_dim=vocab_size, output_dim=embedding_dim))
+    model.add(SimpleRNN(64))
+
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    
+    optimizer = Adam(learning_rate=learning_rate)
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    return model
